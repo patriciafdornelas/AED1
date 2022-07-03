bc4a06af7d1ab26617dfbc719258a9c2f28e4ef9
Patrícia Dornelas <57375053+patriciafdornelas@users.noreply.github.com>
2022-07-03 19:30:55 -0300

    Add files via upload

---
+++ Tarefas/Tarefa7/Ex4/lista_din_enc_ord_rec.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lista_din_enc_ord_rec.h"
+
+struct no{
+    int info;
+    struct no *prox;
+};
+
+Lista cria_lista(){
+    return NULL;
+}
+
+int lista_vazia(Lista lst){
+    if(lst == NULL)
+        return 1;
+    else
+        return 0;
+}
+
+int insere_elem (Lista *lst, int elem){
+    if(lista_vazia(*lst) || elem <= (*lst)->info){
+        Lista N = (Lista)malloc(sizeof(struct no));
+        if(N == NULL)
+            return 0;
+        else{
+            N->info = elem;
+            N->prox = *lst;
+            *lst = N;
+            return 1;
+        }
+    }else{
+        return insere_elem(lst, elem);
+    }   
+    
+}
+
+int remove_elem(Lista *lst, int elem){
+    if(lista_vazia(*lst) == 1 || elem < (*lst)->info)
+        return 0;
+    else{
+        Lista aux = *lst;
+        if(elem == (*lst)->info){
+            *lst = aux->prox;
+            free(aux);
+            return 1;
+        }else{
+            return remove_elem(lst,elem);
+        }
+    }
+}
+
+void imprime(Lista lst){
+    if (lista_vazia(lst) == 1){
+        printf("Erro! Lista vazia! \n");
+    }
+    for (lst;lst != NULL; lst = lst->prox){
+        printf("%d \n", lst->info);
+    }
+}
